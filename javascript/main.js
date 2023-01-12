cb3d0f11fd85afdb3fc261a5d47b2c8cf1ac8ae0
kakisaura <82690252+kakisaura@users.noreply.github.com>
2023-01-11 21:49:29 -0300

    Create function for cards

--- javascript/main.js
+++ javascript/main.js
@@ -30,10 +30,41 @@ function addBookToLibrary(newBook) {
 }
 
 function displayBook() {
-    myLibrary.forEach(element => console.log(element));
+    myLibrary.forEach(element => console.log(element.title));
+    // myLibrary.map(x => console.log(x.title));
 }
 
-// displayBook();
+displayBook();
 
-const cardBook = document.getElementsByClassName('.display');
-console.log(cardBook);
+// const cardBook = document.getElementsByClassName('.display');
+
+// function to display books on card
+const bookContainer = document.querySelector('#book-container');
+
+function createCard() {
+    // create div
+    const displayDiv = document.createElement('div');
+    displayDiv.classList.add('display');
+    bookContainer.appendChild(displayDiv);
+    // create p
+    const paraTitle = document.createElement('p');
+    paraTitle.classList.add('title');
+    displayDiv.appendChild(paraTitle);
+
+    const paraAuthor = document.createElement('p');
+    paraAuthor.classList.add('author');
+    displayDiv.appendChild(paraAuthor);
+
+    const paraPages = document.createElement('p');
+    paraPages.classList.add('pages');
+    displayDiv.appendChild(paraPages);
+
+    const paraStatus = document.createElement('p');
+    paraStatus.classList.add('status');
+    displayDiv.appendChild(paraStatus);
+
+    // paraTitle.textContent = `${myLibrary.title}`
+    // console.log(myLibrary.title);
+}
+
+// createCard();
