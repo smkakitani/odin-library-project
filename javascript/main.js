5d762dd03af90a4ab6b438ced970bbe2fcb5ee61
kakisaura <82690252+kakisaura@users.noreply.github.com>
2023-01-11 23:00:06 -0300

    Fix function creating cards and assign each book to each card

--- javascript/main.js
+++ javascript/main.js
@@ -9,7 +9,7 @@ let myLibrary = [
         title: 'Dune', 
         author: 'Frank Herbert',
         pages: '412',
-        read: true,
+        read: false,
     },
     {
         title: 'Story of the Eye',
@@ -29,8 +29,17 @@ function addBookToLibrary(newBook) {
     myLibrary.push(newBook);
 }
 
+
+
+const bookContainer = document.querySelector('#book-container');
+
 function displayBook() {
-    myLibrary.forEach(element => console.log(element.title));
+    const book = myLibrary.map((element) => {
+        createCard(element);
+    });
+    // console.log(myLibrary);
+
+
     // myLibrary.map(x => console.log(x.title));
 }
 
@@ -39,32 +48,38 @@ displayBook();
 // const cardBook = document.getElementsByClassName('.display');
 
 // function to display books on card
-const bookContainer = document.querySelector('#book-container');
+// const bookContainer = document.querySelector('#book-container');
 
-function createCard() {
+function createCard(currentBook) { //need to assign parameter!!!!!
     // create div
     const displayDiv = document.createElement('div');
     displayDiv.classList.add('display');
     bookContainer.appendChild(displayDiv);
+
     // create p
     const paraTitle = document.createElement('p');
     paraTitle.classList.add('title');
     displayDiv.appendChild(paraTitle);
+    paraTitle.textContent = currentBook.title;
 
     const paraAuthor = document.createElement('p');
     paraAuthor.classList.add('author');
     displayDiv.appendChild(paraAuthor);
+    paraAuthor.textContent = `by ${currentBook.author}`;
 
     const paraPages = document.createElement('p');
     paraPages.classList.add('pages');
     displayDiv.appendChild(paraPages);
+    paraPages.textContent = `${currentBook.pages} pages`;
 
     const paraStatus = document.createElement('p');
     paraStatus.classList.add('status');
     displayDiv.appendChild(paraStatus);
-
-    // paraTitle.textContent = `${myLibrary.title}`
-    // console.log(myLibrary.title);
+    if (currentBook.read === true) {
+        paraStatus.textContent = `finished reading!`
+    } else if (currentBook.read === false) {
+        paraStatus.textContent = `not read yet`
+    }
 }
 
 // createCard();
